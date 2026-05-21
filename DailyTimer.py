ff799059f91d382cb37e773ea33bb67eee528b70
Camila Molinares <camilavm@vt.edu>
2026-05-21 04:30:46 -0400

    Refactored DailyTimer and added embed

---
+++ DailyTimer.py
@@ -0,0 +1,92 @@
+# OS
+import os
+import random
+import datetime
+
+# Time zone
+from zoneinfo import ZoneInfo
+
+# Discord
+import discord
+from discord.ext import commands, tasks
+
+# Translator
+from googletrans import Translator
+
+TIME = datetime.time(hour = 6, minute = 0, tzinfo = ZoneInfo("America/New_York"))
+
+# Timer class
+class DailyTimer(commands.Cog):
+    # Start the timer Cog
+    def __init__(self, bot, filename):
+        self.bot = bot
+        self.filename = filename
+        self.translator = Translator()
+
+        # Load the word list
+        self.words = []
+        with open(self.filename, "r") as f:
+            for line in f:
+                l = line.split(",")
+
+                for word in l:
+                    w = word.strip()
+                    self.words.append(w)
+
+        self.new_word_task.start()
+        
+
+    # Have it run daily
+    #@tasks.loop(time = TIME)
+    @tasks.loop(seconds = 5.0)
+    async def new_word_task(self):
+        # Choose a word and remove it from the list
+        word = random.choice(self.words)
+        self.words.remove(word)
+
+        # Save the remaining words to the text file
+        os.remove(self.filename)
+        with open(self.filename, "w") as f:
+            # Don't overwrite if the list is empty
+            if len(self.words) == 0:
+                print("ERROR: WORD LIST EMPTY")
+                return
+            
+            # First element does not need leading comma
+            for (i, w) in enumerate(self.words, start = 0):
+                if i == 0:
+                    f.write(w)
+                else:
+                    f.write(f",{w}")
+
+        # Translate the word
+        german  = await self.translator.translate(text = word, src = 'en', dest = 'de')
+        spanish = await self.translator.translate(text = word, src = 'en', dest = 'es')
+        telugu  = await self.translator.translate(text = word, src = 'en', dest = 'te')
+
+        # Get the date
+        date = datetime.datetime.today().strftime('%A %d %B %Y')
+
+        # Prepare the response
+        response  = f"\tEnglish: **{word}**\n"
+        response += f"\tGerman: **{german.text}**\n"
+        response += f"\tSpanish: **{spanish.text}**\n"
+        response += f"\tTelugu: **{telugu.text}** (*{telugu.pronunciation}*)\n"
+
+        embed = discord.Embed(
+            title = f"Word of the Day for {date}",
+            description = response,
+            color = discord.Color.green()
+        )
+
+        # Get the channel
+        # TODO: Allow user to select channel
+        for guild in self.bot.guilds:
+            for channel in guild.channels:
+                if channel.name == "general":
+                    view = discord.ui.View()
+                    view.message = await channel.send(
+                        embed = embed,
+                        view = view
+                    )
+                    break
--- bot.py
+++ bot.py
@@ -1,81 +1,18 @@
 # OS
 import os
-import random
-import datetime
-
-# Time zone
-from zoneinfo import ZoneInfo
 
 # Discord
 import discord
-from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-# Translator
-from googletrans import Translator
-
-TIME = datetime.time(hour = 6, minute = 0, tzinfo = ZoneInfo("America/New_York"))
-
-# Timer class
-class DailyTimer(commands.Cog):
-    # Start the timer Cog
-    def __init__(self, bot):
-        self.bot = bot
-        self.translator = Translator()
-        self.new_word_task.start()
-        
-
-    # Have it run daily
-    #@tasks.loop(time = TIME)
-    @tasks.loop(seconds = 5.0)
-    async def new_word_task(self):
-        # Choose a word and remove it from the list
-        word = random.choice(words)
-        words.remove(word)
-
-        # Save the remaining words to the text file
-        os.remove("newwords.txt")
-        with open("newwords.txt", "w") as f:
-            # Don't overwrite if the list is empty
-            if len(words) == 0:
-                print("ERROR: WORD LIST EMPTY")
-                return
-            
-            # First element does not need leading comma
-            for (i, w) in enumerate(words, start = 0):
-                if i == 0:
-                    f.write(w)
-                else:
-                    f.write(f",{w}")
-
-        # Translate the word
-        german  = await self.translator.translate(text = word, src = 'en', dest = 'de')
-        spanish = await self.translator.translate(text = word, src = 'en', dest = 'es')
-        telugu  = await self.translator.translate(text = word, src = 'en', dest = 'te')
-
-        # Prepare the response
-        response = f"Today's word of the day is:\n"
-        response += f"\tEnglish: **{word}**\n"
-        response += f"\tGerman: **{german.text}**\n"
-        response += f"\tSpanish: **{spanish.text}**\n"
-        response += f"\tTelugu: **{telugu.text}** (*{telugu.pronunciation}*)\n"
-
-        # Get the channel
-        # TODO: Allow user to select channel
-        for guild in bot.guilds:
-            for channel in guild.channels:
-                if channel.name == "general":
-                    await channel.send(response)
-                    break
-
-        print(word)
-
+# Project
+from DailyTimer import DailyTimer
 
 # Define the Bot
 class DailyWordsBot(discord.Bot):
     # On ready happens when the bot connects to Discord for the first time
     async def on_ready(self):
-        self.add_cog(DailyTimer(self))
+        self.add_cog(DailyTimer(self, "newwords.txt"))
         print(f'{bot.user} has connected to Discord!')
 
     # Send a message after any message
@@ -91,16 +28,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Set the intents
 intents = discord.Intents.default()
 intents.messages = True
-
-# Load the word list
-words = []
-with open("newwords.txt", "r") as f:
-    for line in f:
-        l = line.split(",")
-
-        for word in l:
-            w = word.strip()
-            words.append(w)
 
 # Initialize the bot
 bot = DailyWordsBot(command_prefix = "&", intents = intents)
