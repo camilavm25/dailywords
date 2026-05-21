f64cc3f846726d90a05894fdfeccfd4e70d2b720
Camila Molinares <camilavm@vt.edu>
2026-05-21 03:58:29 -0400

    Added translation of words and basic formatting

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
+# Translator
+from googletrans import Translator
+
 TIME = datetime.time(hour = 6, minute = 0, tzinfo = ZoneInfo("America/New_York"))
 
 # Timer class
@@ -18,11 +21,13 @@ class DailyTimer(commands.Cog):
     # Start the timer Cog
     def __init__(self, bot):
         self.bot = bot
+        self.translator = Translator()
         self.new_word_task.start()
         
 
     # Have it run daily
-    @tasks.loop(time = TIME)
+    #@tasks.loop(time = TIME)
+    @tasks.loop(seconds = 5.0)
     async def new_word_task(self):
         # Choose a word and remove it from the list
         word = random.choice(words)
@@ -31,21 +36,36 @@ class DailyTimer(commands.Cog):
         # Save the remaining words to the text file
         os.remove("newwords.txt")
         with open("newwords.txt", "w") as f:
+            # Don't overwrite if the list is empty
             if len(words) == 0:
                 print("ERROR: WORD LIST EMPTY")
                 return
             
+            # First element does not need leading comma
             for (i, w) in enumerate(words, start = 0):
                 if i == 0:
                     f.write(w)
                 else:
                     f.write(f",{w}")
 
+        # Translate the word
+        german  = await self.translator.translate(text = word, src = 'en', dest = 'de')
+        spanish = await self.translator.translate(text = word, src = 'en', dest = 'es')
+        telugu  = await self.translator.translate(text = word, src = 'en', dest = 'te')
+
+        # Prepare the response
+        response = f"Today's word of the day is:\n"
+        response += f"\tEnglish: **{word}**\n"
+        response += f"\tGerman: **{german.text}**\n"
+        response += f"\tSpanish: **{spanish.text}**\n"
+        response += f"\tTelugu: **{telugu.text}** (*{telugu.pronunciation}*)\n"
+
         # Get the channel
+        # TODO: Allow user to select channel
         for guild in bot.guilds:
             for channel in guild.channels:
                 if channel.name == "general":
-                    await channel.send(f"Today's word of the day is: {word}")
+                    await channel.send(response)
                     break
 
         print(word)
